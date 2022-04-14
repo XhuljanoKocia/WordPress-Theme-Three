d866496cb87467fc0710c85ebc7cf2285e3fbe7d
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-04-14 23:24:50 +0200

    The three building blocks of a WordPress theme (part 1)

--- index.php
+++ index.php
@@ -6,7 +6,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Berserk Home</title>
 </head>
-<body>
+<body <?php body_class(); ?>>
     <p>This is just a paragraph!</p>
+    <header>
+        <section class="top-bar">
+            <div class="social-media-icons"></div>
+            <div class="search"></div>
+        </section>
+        <section class="menu-area">
+            <section class="logo"></section>
+            <nav class="main-menu"></nav>
+        </section>
+    </header>
+    <div class="content-area">
+        <main>
+            <section class="slide"></section>
+            <section class="service"></section>
+            <section class="middle-area">
+                <aside class="sidebar"></aside>
+                <div class="news"></div>
+            </section>
+            <section class="map"></section>
+        </main>
+    </div>
+    <footer></footer>
 </body>
 </html>
