b6a63f90cfab21f08da5723cc9fcbed53d1e4137
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-04-23 12:27:22 +0200

    Including Bootstrap 5 (part 1)

--- functions.php
+++ functions.php
@@ -1,6 +1,9 @@
 <?php
+    // Including stylesheet and script files
     function load_scripts(){
         wp_enqueue_style('template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all');
+        wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.1.3', true);
+        wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css', array(), '5.1.3', 'all');
     }
     add_action('wp_enqueue_scripts', 'load_scripts');
 ?>
