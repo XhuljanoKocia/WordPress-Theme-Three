ac08c6c369c23a87bf39660f96b0a0d6fe752985
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-04-25 14:28:28 +0200

    What are and how to use Template Pages (part 1)

--- page-about-us.php
+++ page-about-us.php
@@ -1,16 +1,6 @@
 <?php get_header(); ?>
     <div class="content-area">
         <main>
-            <section class="slide">
-                <div class="container">
-                    <div class="row">Slide</div>
-                </div>
-            </section>
-            <section class="service">
-                <div class="container">
-                    <div class="row">Services</div>
-                </div>
-            </section>
             <section class="middle-area">
                 <div class="container">
                     <div class="row">
@@ -36,11 +26,6 @@
                             <?php endif; ?>
                         </div>
                     </div>
-                </div>
-            </section>
-            <section class="map">
-                <div class="container">
-                    <div class="row">Map</div>
                 </div>
             </section>
         </main>
