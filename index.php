d5eea4630999c2dadfb869e279a28a05932ffe04
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-04-29 11:09:22 +0200

    Getting template parts

--- index.php
+++ index.php
@@ -21,16 +21,7 @@
                                 if(have_posts()):
                                     while(have_posts()):
                                         the_post();
-                            ?>
-                            <article>
-                                <h2><?php the_title(); ?></h2>
-                                <?php the_post_thumbnail(array(275, 275)); ?>
-                                <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-                                <p>Categories: <?php the_category(' '); ?></p>
-                                <p><?php the_tags('Tags: ', ', ') ?></p>
-                                <p><?php the_content(); ?></p>
-                            </article>
-                            <?php
+                                    get_template_part('template-parts/content');
                                     endwhile;
                                 else:
                             ?>
