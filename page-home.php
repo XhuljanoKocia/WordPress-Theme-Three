95d0d91551d7a4baffceafb551994c5e006fa1e9
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-05-04 16:37:30 +0200

    The powerful WP_Query class (part 3)

--- page-home.php
+++ page-home.php
@@ -53,6 +53,11 @@
                                         if($featured->have_posts()):
                                             while($featured->have_posts()):
                                                 $featured->the_post();
+                                    ?>
+                                    <div class="col-12">
+                                        <?php get_template_part('template-parts/content', 'featured'); ?>
+                                    </div>
+                                    <?php
                                             endwhile;
                                             wp_reset_postdata();
                                         endif;
